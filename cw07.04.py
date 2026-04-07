bae099c5e2358e0583f74a20800a9a68eabe5e89
ChumakOleksandr <cddfg714@gmail.com>
2026-04-07 20:31:39 +0300

    cw07.04 task 2(not full)

--- cw07.04.py
+++ cw07.04.py
@@ -17,21 +17,21 @@ from datetime import datetime
 
 class Message:
     def __init__(self, user: str, text: str, time_str: str):
-        self.__user = user
-        self.__text = text
-        self.__time = datetime.strptime(time_str, "%H:%M")
+        self._user = user
+        self._text = text
+        self._time = datetime.strptime(time_str, "%H:%M")
 
     def __str__(self):
-        return f"[{self.__time.strftime('%H:%M')}] {self.__user}: {self.__text}"
+        return f"[{self._time.strftime('%H:%M')}] {self._user}: {self._text}"
 
     def __len__(self):
-        return len(self.__text)
+        return len(self._text)
 
     def __gt__(self, other):
-        return self.__time < other.__time  # менший час = старіше повідомлення
+        return self._time < other.__time  # менший час = старіше повідомлення
 
     def get_time(self):
-        return self.__time
+        return self._time
 
 
 messages = [
@@ -51,3 +51,56 @@ for msg in messages:
     print(msg)
 
 print("\nДовжина тексту першого повідомлення:", len(messages[0]))
+
+# Завдання 2
+# Створіть клас Song з атрибутами
+#  name – назва пісні
+#  author – ім’я автора
+# методи:
+#  __eq__(self, other) – перевіряє чи дві пісні однакові
+#  __str__(self, other) – повертає рядок з назвою та автором
+# Створіть клас Playlist з атрибутами
+#  songs – список пісень(об’єкти класу Song)
+# методи:
+#  __len__(self) – повертає кількість пісень
+#  __contains__(self, item) – перевіряє чи є пісня в плейлисті
+#  __iter__(self) – повертає літератор для циклу for
+#  add_song(self, song) – додає пісню в плейлист
+#  remove_song(self, song) – видаляє пісню з плейлиста
+# Створіть порожній плейлист
+# Створіть 3 пісні:
+# "Imagine", "John Lennon"
+# "Bohemian Rhapsody", "Queen"
+# "Shape of You", "Ed Sheeran"
+# Добавте їх в плейлист
+# Пройдіться циклом for по плейлисту та виведіть кожну
+# пісню на екран
+
+
+class Song:
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __eq__(self, other):
+        return (
+            self._name.lower() == other._name.lower()
+            and self._author.lower() == other._author.lower()
+        )
+
+    def __str__(self):
+        return f"{self._name} — {self._author}"
+
+
+class Playlist:
+    def __init__(self):
+        self._songs = []
+
+    def __len__(self):
+        return len(self._songs)
+
+    def __contains__(self, item: Song):
+        return item in self._songs
+
+    def __iter__(self):
+        return iter(self._songs)
