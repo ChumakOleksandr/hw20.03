309d0d859f7e925c9cfaf8545bdaa6f573c1507f
ChumakOleksandr <cddfg714@gmail.com>
2026-04-23 20:36:40 +0300

    cw23.04(task2)

--- cw23.04.py
+++ cw23.04.py
@@ -79,3 +79,73 @@ login_system(users, "ivan", "12345")
 change_password(users, "ivan", "newpass")
 
 delete_user(users, "olga")
+
+# Завдання 2
+# Створіть клас Cart
+# Атрибути:
+#  user – ім’я користувача
+#  items – список товарів
+#  total – загальна ціна
+# Методи:
+#  add(item, price) – добавити товар у кошик
+#  delete(item, price) – видалити товар з кошика
+#  info() – вивести інформацію про кошик
+#  save(fiename) – зберегти дані у файл(за
+# замовчуванням cart.json)
+#  load(fiename) – завантажити дані з файла(за
+# замовчуванням cart.json)
+
+
+class Cart:
+    def __init__(self, user: str):
+        self._user: str = user
+        self._items: list[str] = []
+        self._total: float = 0.0
+
+    def add(self, item: str, price: float) -> None:
+        self._items.append(item)
+        self._total += price
+
+    def delete(self, item: str, price: float) -> None:
+        if item in self._items:
+            self._items.remove(item)
+            self._total -= price
+            if self._total < 0:
+                self._total = 0
+        else:
+            print("Товару немає у кошику")
+
+    def info(self) -> None:
+        print(f"Користувач: {self._user}")
+        print(f"Товари: {self._items}")
+        print(f"Загальна сума: {self._total}")
+
+    def save(self, filename: str = "cart.json") -> None:
+        data = {
+            "user": self._user,
+            "items": self._items,
+            "total": self._total,
+        }
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+    def load(self, filename: str = "cart.json") -> None:
+        with open(filename, encoding="utf-8") as f:
+            data = json.load(f)
+
+        self._user = data["user"]
+        self._items = data["items"]
+        self._total = data["total"]
+
+
+art = Cart("Іван")
+
+art.add("Хліб", 25)
+art.add("Молоко", 40)
+
+art.info()
+art.save()
+
+new_cart = Cart("")
+new_cart.load()
+new_cart.info()
