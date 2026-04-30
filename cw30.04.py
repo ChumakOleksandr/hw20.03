3428fb45d860a27a36a721fa91d04641bd8dba20
ChumakOleksandr <cddfg714@gmail.com>
2026-04-30 19:54:26 +0300

    cw(task1_remake)

--- cw30.04.py
+++ cw30.04.py
@@ -6,28 +6,32 @@ import threading
 # у списку. Результати обчислень виведіть на екран.
 
 
-def find_max(numbers: list[int]) -> None:
-    max_value = max(numbers)
-    print(f"Максимальне значення: {max_value}")
+def find_max(numbers: list[int], result: dict[str, int]) -> None:
+    result["max"] = max(numbers)
 
 
-def find_min(numbers: list[int]) -> None:
-    min_value = min(numbers)
-    print(f"Мінімальне значення: {min_value}")
+def find_min(numbers: list[int], result: dict[str, int]) -> None:
+    result["min"] = min(numbers)
 
 
 def main() -> None:
-    user_input = input("Введіть числа через пробіл: ")
-    numbers = [int(num) for num in user_input.split()]
+    input_text = input("Введіть числа через пробіл: ")
+    string_numbers = input_text.split()
+    numbers = [int(number) for number in string_numbers]
 
-    t1 = threading.Thread(target=find_max, args=(numbers,))
-    t2 = threading.Thread(target=find_min, args=(numbers,))
+    results: dict[str, int] = {}
 
-    t1.start()
-    t2.start()
+    max_thread = threading.Thread(target=find_max, args=(numbers, results))
+    min_thread = threading.Thread(target=find_min, args=(numbers, results))
 
-    t1.join()
-    t2.join()
+    max_thread.start()
+    min_thread.start()
+
+    max_thread.join()
+    min_thread.join()
+
+    print(f"Максимальне значення: {results['max']}")
+    print(f"Мінімальне значення: {results['min']}")
 
 
 if __name__ == "__main__":
